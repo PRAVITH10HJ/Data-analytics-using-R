d93a107c74a11071e0d26eb388ebb95b5fc97417
Pravith S Upadhyaya <151915860+PRAVITH10HJ@users.noreply.github.com>
2024-09-23 12:32:14 +0530

    Add files via upload

---
+++ 23-9-24 data frame.R
@@ -0,0 +1,9 @@
+uid=c(1001,1002,1003,1004)
+uname=c("abhi","bhuvi","charlie","drake")
+users=data.frame("ID"=uid,"NAME"=uname,stringsAsFactors = TRUE)
+users
+str(users)
+summary(users)
+users$NAME
+users$u_location=c("blr",'mub','chn','amd')
+users
